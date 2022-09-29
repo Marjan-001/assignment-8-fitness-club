bc2d2749ac9e2bb5197a7ef3f0c895f434b20d6f
Marjan-001 <marjan199721@gmail.com>
2022-09-29 11:15:43 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <h1> Fitness Club</h1>
+            </div>
+
+
+        </div>
+
+
+    );
+};
+
+export default Header;
